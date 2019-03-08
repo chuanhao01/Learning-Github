a329f72c5511d81e00a0dde020d43dfab488e11b
chuanhao01 <chuanhaolim@gmail.com>
2019-03-08 22:37:42 +0800

    New Test for get and request

--- Python/Google sheets combine with data anylsis project/Main code for work.py
+++ Python/Google sheets combine with data anylsis project/Main code for work.py
@@ -13,19 +13,15 @@ gc = gspread.authorize(credemtials) #gc is now your google account, with the acc
 # on google sheets, also the method .sheetx is to specify which sheet, number.
 wks = gc.open('Test Data for github test')
 
-# Testing some stuff, will write doc later
-sheet1 = wks.worksheet("Sheet1")
-sheet1Dataframe = pd.DataFrame(sheet1.get_all_records())
-print(sheet1Dataframe)
 
 sheet2 = wks.worksheet("Sheet2")
-print(sheet2.get_all_records())
-sheet2Dataframe = pd.DataFrame(sheet2.get_all_records())
+print(sheet2.get_all_values())
+sheet2Dataframe = pd.DataFrame(sheet2.get_all_values())
 print(sheet2Dataframe)
-test_list = sheet2.range("A1:F2")
-print(test_list)
-print(type(test_list))
-print(type(test_list[0]))
-print(test_list[0])
-test_list[0].value = "Z"
-print(test_list[0])
+listOfValuesToAdd = sheet2.range("A3:F3")
+print(listOfValuesToAdd)
+for i in listOfValuesToAdd:
+    i.value = 200
+sheet2.update_cells(listOfValuesToAdd)
+sheet2Dataframe = pd.DataFrame(sheet2.get_all_values())
+print(sheet2Dataframe)
